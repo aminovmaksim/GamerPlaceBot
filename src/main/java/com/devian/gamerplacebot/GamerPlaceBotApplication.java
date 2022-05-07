9d143574f3f2491498096230819ea9d23d98e1c9
Max Aminov <aminovmaksim@gmail.com>
2022-05-08 00:11:52 +0300

    tomcat added

--- src/main/java/com/devian/gamerplacebot/GamerPlaceBotApplication.java
+++ src/main/java/com/devian/gamerplacebot/GamerPlaceBotApplication.java
@@ -2,9 +2,10 @@ package com.devian.gamerplacebot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GamerPlaceBotApplication {
+public class GamerPlaceBotApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(GamerPlaceBotApplication.class, args);
