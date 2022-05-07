4661af3b5dafec8575e349a204148d69624c85e7
Max Aminov <aminovmaksim@gmail.com>
2022-05-07 19:01:16 +0300

    test update

--- src/test/java/com/devian/gamerplacebot/GamerPlaceBotApplicationTests.java
+++ src/test/java/com/devian/gamerplacebot/GamerPlaceBotApplicationTests.java
@@ -1,13 +1,9 @@
 package com.devian.gamerplacebot;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
 class GamerPlaceBotApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
