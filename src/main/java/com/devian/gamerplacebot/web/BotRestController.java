78cf11be1e5a25d619b2dc3d3eeca5f2cd09b849
Max Aminov <aminovmaksim@gmail.com>
2022-05-18 22:46:05 +0300

    updated map

--- src/main/java/com/devian/gamerplacebot/web/BotRestController.java
+++ src/main/java/com/devian/gamerplacebot/web/BotRestController.java
@@ -4,6 +4,7 @@ import com.devian.gamerplacebot.data.DataAccess;
 import com.devian.gamerplacebot.data.model.ClubInfo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class BotRestController {
 
     private final DataAccess dataAccess;
 
+    @CrossOrigin({"http://localhost:63342", "https://aminovmaksim.github.io"})
     @GetMapping(value = "/clubs", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<ClubInfo> getClubList() {
         return dataAccess.clubDao.getClubList();
